abd14d1e0ed496b9b2da7fe2e94b7f9b927280cc
latilanoh@gmail.com <latilanoh@gmail.com>
2018-06-17 20:43:59 -0500

    se inicia con la parte del modal para inicio de sesión, en el nav se genera el href para dirigir al modal, en el archivo js se inicializa el modal según documentacion de materizalize y en la estructura html se inserta un bloque inicial de prueba

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-// La funcionalidad de tu proyecto
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+});
